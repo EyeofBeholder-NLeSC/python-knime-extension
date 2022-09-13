e4320fab6f900c300349493666923a9f3fc794de
Ji Qi <92043159+jiqicn@users.noreply.github.com>
2022-09-13 16:34:37 +0200

    Add unit tests to the execute function of the validator node. The validator needs a virtual backend (variable `_backend`, defined as None in the knime_extension library), and it should be an instance of the abstract class named _Backend. To add unit test to the execute function, we made the _Backend class concrete, and override the `create_table_from_pandas` method by returning the exact pandas data frame.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,6 +1,9 @@
 import os
 import sys
+from unittest.mock import patch
 import pytest
+from knime_extension.knime_node_table import _Backend
+import knime_extension
 
 
 extension_path = os.path.abspath("../extension")
@@ -8,6 +11,11 @@ sys.path.insert(0, extension_path)
 
 from my_extension import CSVWValidator as cv
 from my_extension import validate_metadata_url
+
+
+class ConcreteBackend(_Backend):
+    def create_table_from_pandas(self, data, sentinel):
+        return data
 
 
 def test_validate_metadata_url():
@@ -29,3 +37,25 @@ def test_validate_metadata_url():
     # url not linking to a json file
     with pytest.raises(Exception) as e:
         validate_metadata_url("https://www.google.com/maps")
+
+
+def test_validator_execute():
+    validator = cv()
+
+    # negative test
+    with pytest.raises(AssertionError) as e:
+        validator.metadata_url = (
+            "https://w3c.github.io/csvw/tests/test111-metadata.json"
+        )
+        validator.execute(None)
+
+    # check output
+    knime_extension.knime_node_table._backend = ConcreteBackend()
+    validator.metadata_url = "https://w3c.github.io/csvw/tests/countries.json"
+    r = validator.execute(None)
+    assert r.shape[0] == 2
+    assert (
+        r["csv_urls"].iloc[0] == "https://w3c.github.io/csvw/tests/countries.csv"
+        and r["csv_urls"].iloc[1]
+        == "https://w3c.github.io/csvw/tests/country_slice.csv"
+    )
